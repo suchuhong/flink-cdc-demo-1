602f3405d6033b7f5d21ba3cc210f31900321a0a
suchuhong <1442615457@qq.com>
2024-10-27 17:50:14 +0800

    init

---
+++ src/main/java/org/example/CustomSink.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.apache.flink.api.common.eventtime.Watermark;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+
+public class CustomSink extends RichSinkFunction<String> {
+    @Override
+    public void invoke(String value, Context context) throws Exception {
+        System.out.println(">>> " + value);
+    }
+
+    @Override
+    public void writeWatermark(Watermark watermark) throws Exception {
+        super.writeWatermark(watermark);
+    }
+
+    @Override
+    public void finish() throws Exception {
+        super.finish();
+    }
+}
